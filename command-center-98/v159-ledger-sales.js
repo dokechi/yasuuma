1bfd9836c0ddd4d73bde7d7fca305ee75fe770b1
dokechi <mekazawakun2525@gmail.com>
2026-09-14 06:05:14 +0900

    Reduce Mizuno M white inventory by one

--- command-center-98/v159-ledger-sales.js
+++ command-center-98/v159-ledger-sales.js
@@ -2,7 +2,7 @@
   const n=s=>String(s||'').normalize('NFKC').toUpperCase().replace(/\s+/g,' ');
   const money=v=>'¥'+Number(v||0).toLocaleString('ja-JP');
   const mizunoMockSold={
-    'M|WHITE':3,'M|BLACK':2,'M|NAVY':4,
+    'M|WHITE':4,'M|BLACK':2,'M|NAVY':4,
     'L|WHITE':3,'L|BLACK':4,'L|NAVY':2,
     'XL|WHITE':5,'XL|BLACK':1,'XL|NAVY':2,
     '2XL|WHITE':0,'2XL|BLACK':1,'2XL|NAVY':0
